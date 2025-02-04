9a6f1cf6c8f59d38bd951b48af25834c6a71df8d
JunYang <JUNYANG.23@ichat.sp.edu.sg>
2025-02-05 02:27:57 +0800

    points UI

--- php/points.php
+++ php/points.php
@@ -30,29 +30,43 @@ $_SESSION['prev_page'] = $_SERVER['REQUEST_URI'];
     <div class = "pointtext" >Current Points</div>
     <div class="container">
         <div class="slider">
-            <img src = "../images/Limited.png" alt= "third" class = "image1" />
-            <img src = "../images/pastaredeem.png" alt= "third" class = "image1"/>
-            <img src = "../images/cheeseredeem.png" alt= "third" class  = "image1"/>
-            <img src = "../images/chickenredeem.png" alt= "third" class = "image1"/>
+            <img src = "../images/Limited.png" alt= "third" class = "image1" data-points="400" />
+            <img src = "../images/pastaredeem.png" alt= "third" class = "image1" data-points="450"/>
+            <img src = "../images/cheeseredeem.png" alt= "third" class  = "image1" data-points="300"/>
+            <img src = "../images/chickenredeem.png" alt= "third" class = "image1" data-points="250"/>
             <img src = "../food_images/drink.png" a lt= "third" class  = "image1"/>
             <img src = "../food_images/drink.png" alt= "third" class = "image1"/>
             <img src = "../food_images/drink.png" alt= "third" class  = "image1"/>
         </div>
     </div>  
-    <div id="popup" class="popup">
-  <div class="popup-content">
-    <img id="popupimage" src="" alt="image">
-    <p>Are you sure you want to redeem this product?</p>
-    <button id="confirmBtn">Confirm</button>
-    <button id="cancelBtn">Cancel</button>
-    <div class = "userpoint">Your Current Points are</div>
-    <div class = "costpoint">This redemption costs</div>
-    <div class = "pointafter">Your points after redemption</div>
-    <div class = "point1"></div>
-    <div class = "point2"></div>
-    <div class = "point3"></div>
+  <div id="popup" class="popup">
+    <div class="popup-content">
+      <img id="popupimage" src="" alt="image">
+      <p>Are you sure you want to redeem this product?</p>
+      <button id="confirmBtn">Confirm</button>
+      <button id="cancelBtn">Cancel</button>
+      <div class = "userpoint">Your Current Points are :</div>
+      <div class = "costpoint">Points deducted for redemption :</div>
+      <div class = "pointafter">Your points after redemption :</div>
+      <div id = "point1">error</div>
+      <div id = "point2">error</div>
+      <div id = "point3">error</div>
+    </div>
   </div>
-</div>
+  <div class = "cont1">
+    <img src = "../food_images/fries.png" alt= "third" id = "img1" data-points="150"/>
+    <div class = "label1">Chili Pepper Fries</div>
+    <div class = "desc1">Fresh peeled french fries, seasoned with our homemade chili pepper. Guaranteed Spice and Flavour!</div>
+    <button id="redeem1">Redeem</button>
+  </div>
+  <div class = "cont2">
+    <img src = "../food_images/seasonalnobg.png" alt= "third" id = "img2" data-points="500"/>
+    <div class = "label2">Punny Pepperoni Paradise</div>
+    <div class = "desc2">This pepperoni pizza packs a punch with zesty tomato sauce, a mountain of pepperoni slices and cheesy layers.
+      A paradise of flavor that’ll have you coming back for more puns.</div>
+    <button id="redeem2">Redeem</button>
+  </div>
+
   
 
 </body>
@@ -71,12 +85,15 @@ $_SESSION['prev_page'] = $_SERVER['REQUEST_URI'];
   const confirmBtn = document.getElementById('confirmBtn');
   const cancelBtn = document.getElementById('cancelBtn');
   const popupimage = document.getElementById('popupimage');
-  let selectedImage;
-
+  const redeem1 = document.getElementById('redeem1');
+ 
   container.addEventListener('click', function(event) {
     if (event.target.classList.contains('image1')) {
+      let selectedImage;
       selectedImage = event.target;
       popupimage.src = selectedImage.src;
+      const points = selectedImage.getAttribute('data-points');
+      document.getElementById("point2").innerHTML = points;
       popup.style.display = 'block';
     }
   });
@@ -89,5 +106,24 @@ $_SESSION['prev_page'] = $_SERVER['REQUEST_URI'];
   cancelBtn.addEventListener('click', function() {
     popup.style.display = 'none';
   });
+
+  redeem1.addEventListener('click', function() {
+    let selectedImage;
+    selectedImage = document.getElementById("img1")
+    popupimage.src = selectedImage.src;
+    points = selectedImage.getAttribute('data-points');
+    document.getElementById("point2").innerHTML = points;
+    popup.style.display = 'block';
+  });
+
+  redeem2.addEventListener('click', function() {
+    let selectedImage;
+    selectedImage = document.getElementById("img2")
+    popupimage.src = selectedImage.src;
+    points = selectedImage.getAttribute('data-points');
+    document.getElementById("point2").innerHTML = points;
+    popup.style.display = 'block';
+  });
+
 </script>
 </html>
